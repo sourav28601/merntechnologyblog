0fd7182773e913b000a8911b2c2da3c9377dc4ec
sourav28601 <96834403+sourav28601@users.noreply.github.com>
2022-08-19 00:24:17 +0530

    .

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(session({
   cookie: { secure: false }
 }));
 app.use(flash())
-const port = process.env || 3000
+const port = process.en.PORT || 3000
 
 const web = require('./routes/web.js')   // ROUTER LINK
 
